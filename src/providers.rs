39d6c50f56e75c35e2a64d92257eb89613f5df99
Yaser Martinez Palenzuela <yaser.martinez@gmail.com>
2022-03-24 20:01:52 +0100

    add get_block

--- src/providers.rs
+++ src/providers.rs
@@ -3,6 +3,7 @@ use ethers::providers::Http;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
 use pyo3_asyncio::tokio::future_into_py;
+use pythonize::pythonize;
 use std::convert::TryFrom;
 use std::sync::Arc;
 
@@ -17,7 +18,6 @@ pub fn to_py_exception(err: impl std::fmt::Display) -> PyErr {
 
 #[pymethods]
 impl HTTPProvider {
-
     #[new]
     pub fn new(endpoint: String) -> Self {
         let provider = ethers::providers::Provider::<Http>::try_from(endpoint.as_str()).unwrap();
@@ -25,6 +25,18 @@ impl HTTPProvider {
             provider: Arc::new(provider),
         }
     }
+
+    pub fn get_block<'p>(&self, py: Python<'p>, block_number: u64) -> PyResult<&'p PyAny> {
+        let provider = Arc::clone(&self.provider);
+        future_into_py(py, async move {
+            let block = provider
+                .get_block(block_number)
+                .await
+                .map_err(to_py_exception)?;
+            Ok(Python::with_gil(|py| pythonize(py, &block).unwrap()))
+        })
+    }
+
 
     /// Decode the given list of tokens to a final string Gets the latest block number via the `eth_BlockNumber` API
     ///
@@ -37,7 +49,11 @@ impl HTTPProvider {
     pub fn get_block_number<'p>(&self, py: Python<'p>) -> PyResult<&'p PyAny> {
         let provider = Arc::clone(&self.provider);
         future_into_py(py, async move {
-            let bn = provider.get_block_number().await.map_err(to_py_exception)?.as_u64();
+            let bn = provider
+                .get_block_number()
+                .await
+                .map_err(to_py_exception)?
+                .as_u64();
             Ok(Python::with_gil(|py| bn.to_object(py)))
         })
     }
--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,10 +1,21 @@
 import pytest
 from ethers.providers import HTTPProvider
 
+ENDPOINT = "https://mainnet.infura.io/v3/670cc38391324e12bbcf893d84c175c3"
+
 
 async def test_get_block_number() -> None:
-    provider = HTTPProvider(
-        "https://mainnet.infura.io/v3/670cc38391324e12bbcf893d84c175c3"
-    )
+    provider = HTTPProvider(ENDPOINT)
     block_number = await provider.get_block_number()
     assert block_number > 0
+
+
+async def test_get_block() -> None:
+    provider = HTTPProvider(ENDPOINT)
+
+    # https://etherscan.io/txs?block=14430335&p=5
+    block_number = await provider.get_block(14430335)
+    assert (
+        block_number["transactions"][0]
+        == "0x9d6d096e442c7725679c81fb7068919e1027d2a7b3da7d96383039d4cd9341e5"
+    )
