b19d5c44e4f56293e3302f54cbec9a5995c4fe63
Yaser Martinez Palenzuela <yaser.martinez@gmail.com>
2022-04-04 23:21:14 +0200

    drop support for python 3.6

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     extras_require=extras,
     classifiers=[
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",                
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
